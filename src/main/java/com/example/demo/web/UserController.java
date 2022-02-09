840945fa5718e1a3f94c4b21c80af51c22f9a082
Silvio Esterilla <silvioesterilla@MacBook-Pro-de-Silvio-2.local>
2022-02-09 14:38:02 -0500

    commit inicial

---
+++ src/main/java/com/example/demo/web/UserController.java
@@ -0,0 +1,29 @@
+package com.example.demo.web;
+
+import com.example.demo.model.UserDTO;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("usuario")
+public class UserController {
+
+  private static List<UserDTO> usuarios = new ArrayList<>();
+
+  @PostMapping()
+  public UserDTO guardarUsuario( @RequestBody UserDTO usuario ) {
+    usuario.setId(usuarios.size() + 1);
+    usuarios.add( usuario );
+    return usuario;
+  }
+
+  @GetMapping()
+  public  List<UserDTO> getUsuarios(){
+    return usuarios;
+  }
+}
